06c656142aa08fe8b92d6a35f4aff4a91de4a805
zor-cell <philippemanu.zorman@gmail.com>
2025-03-30 11:25:21 +0200

    Initial commit

---
+++ src/main/java/net/zorphy/backend/controller/GreetingController.java
@@ -0,0 +1,12 @@
+package net.zorphy.backend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+    @RequestMapping("/")
+    public String getGreeting() {
+        return "Hello world!";
+    }
+}
