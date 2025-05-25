f93022f4d60a989be3a8d9b3c2b97670c90c545a
zor-cell <philippemanu.zorman@gmail.com>
2025-05-25 22:21:31 +0200

    Added projects component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,8 @@
 import { Routes } from '@angular/router';
+import {Connect4Component} from "./components/connect4/connect4.component";
+import {ProjectsComponent} from "./components/projects/projects.component";
 
 export const routes: Routes = [
-    {path: 'connect4', component: 'null'}
+    {path: 'projects', component: ProjectsComponent},
+    {path: 'connect4', component: Connect4Component}
 ];
---
+++ frontend/src/app/components/projects/projects.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
+
+@Component({
+  selector: 'app-projects',
+  imports: [RouterLink],
+  templateUrl: './projects.component.html',
+  standalone: true,
+  styleUrl: './projects.component.css'
+})
+export class ProjectsComponent {
+
+}
