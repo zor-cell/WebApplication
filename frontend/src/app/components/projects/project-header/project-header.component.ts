de487f60511fed2a47fc942c330e8ff4dfb25ce2
zor-cell <philippemanu.zorman@gmail.com>
2025-06-20 21:52:29 +0200

    basic frontend changes

--- frontend/src/app/components/projects/project-header/project-header.component.ts
+++ frontend/src/app/components/projects/project-header/project-header.component.ts
@@ -2,6 +2,7 @@ import {Component, Input} from '@angular/core';
 import {Location, NgIf} from "@angular/common";
 import {RouterLink} from "@angular/router";
 import {ProjectMetadata} from "../../../dto/projects/responses";
+import {AuthService} from "../../../services/auth.service";
 
 @Component({
   selector: 'app-project-header',
@@ -16,8 +17,12 @@ import {ProjectMetadata} from "../../../dto/projects/responses";
 export class ProjectHeaderComponent {
   @Input() project: ProjectMetadata| undefined = undefined;
   @Input() showWebsite: boolean = false;
+  @Input() showEdit: boolean = false;
 
-  constructor(private location: Location) {}
+  constructor(private location: Location,
+              public authService: AuthService) {
+    console.log(authService.user, authService.isAdmin())
+  }
 
   routeBack() {
     this.location.back();
