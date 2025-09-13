8824cf6b5923f4b94dbc0acc84d10f6a5ce6b165
zor-cell <philippemanu.zorman@gmail.com>
2025-09-13 19:49:19 +0200

    updated image mapping in markdown content

--- backend/src/main/java/net/zorphy/backend/main/component/FileReaderComponent.java
+++ backend/src/main/java/net/zorphy/backend/main/component/FileReaderComponent.java
@@ -1,6 +1,6 @@
 package net.zorphy.backend.main.component;
 
-import org.commonmark.node.Node;
+import org.commonmark.node.*;
 import org.commonmark.parser.Parser;
 import org.commonmark.renderer.html.HtmlRenderer;
 import org.mapstruct.Named;
@@ -10,19 +10,46 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Component
 public class FileReaderComponent {
     private final ResourceLoader resourceLoader;
     private final Parser parser;
     private final HtmlRenderer renderer;
+    private final FileUrlComponent fileUrlComponent;
+    private final Pattern imgSrcPattern = Pattern.compile("src=\"([^\"]+)\"");
 
-    public FileReaderComponent(ResourceLoader resourceLoader) {
+    public FileReaderComponent(ResourceLoader resourceLoader, FileUrlComponent fileUrlComponent) {
         this.resourceLoader = resourceLoader;
         parser = Parser.builder()
                 .build();
         renderer = HtmlRenderer.builder()
                 .build();
+        this.fileUrlComponent = fileUrlComponent;
+    }
+
+    @Named("readHtmlFromMarkdown")
+    public String readHtmlFromMarkdown(String filePath) {
+        String content = readContent(filePath);
+
+        Node document = parser.parse(content);
+        document.accept(new AbstractVisitor() {
+            @Override
+            public void visit(HtmlBlock htmlBlock) {
+                String literal = htmlBlock.getLiteral();
+                htmlBlock.setLiteral(rewriteImgSrc(literal));
+                super.visit(htmlBlock);
+            }
+        });
+
+        return renderer.render(document);
+    }
+
+    @Named("readTextFromMarkdown")
+    public String readTextFromMarkdown(String filePath, int limit) {
+        return readContent(filePath);
     }
 
     private String readContent(String filePath) {
@@ -38,16 +65,24 @@ public class FileReaderComponent {
         }
     }
 
-    @Named("readHtmlFromMarkdown")
-    public String readHtmlFromMarkdown(String filePath) {
-        String content = readContent(filePath);
+    private String rewriteImgSrc(String html) {
+        //rewrite html src="" tags with correct file path
+        Matcher matcher = imgSrcPattern.matcher(html);
+        StringBuilder sb = new StringBuilder();
 
-        Node document = parser.parse(content);
-        return renderer.render(document);
-    }
+        while (matcher.find()) {
+            String filePath = matcher.group(1);
 
-    @Named("readTextFromMarkdown")
-    public String readTextFromMarkdown(String filePath, int limit) {
-        return readContent(filePath);
+            int index = filePath.indexOf("static/");
+            if(index != -1){
+                filePath = filePath.substring(index + "static/".length());
+            }
+
+            String replacement = "src=\"" + fileUrlComponent.resolveStaticUrl(filePath) + "\"";
+            matcher.appendReplacement(sb, Matcher.quoteReplacement(replacement));
+        }
+        matcher.appendTail(sb);
+
+        return sb.toString();
     }
 }
