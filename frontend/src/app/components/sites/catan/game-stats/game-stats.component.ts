f9cd43de16c5234993b9a4326d909a25cf038dea
zor-cell <philippemanu.zorman@gmail.com>
2025-10-17 13:47:17 +0200

    minor frontend fixes

--- frontend/src/app/components/sites/catan/game-stats/game-stats.component.ts
+++ frontend/src/app/components/sites/catan/game-stats/game-stats.component.ts
@@ -2,11 +2,13 @@ import {Component, input} from '@angular/core';
 import {GameStats} from "../../../../dto/sites/catan/game/GameStats";
 import {CatanHistogramComponent} from "../histogram/histogram.component";
 import {GameType} from "../../../../dto/games/GameType";
+import {GameStatsMetaComponent} from "../../../games/stats/game-stats-meta/game-stats-meta.component";
 
 @Component({
     selector: 'catan-game-stats',
     imports: [
-        CatanHistogramComponent
+        CatanHistogramComponent,
+        GameStatsMetaComponent
     ],
     templateUrl: './game-stats.component.html',
     styleUrl: './game-stats.component.css',
